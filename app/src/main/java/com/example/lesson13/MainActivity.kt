bf13d3ee5c779220072572252ec0caa6447fd21a
MarkYahorov <markgoodboy@mail.ru>
2021-06-14 16:18:54 +0300

    All logic: add recycler and listeners in viewHolder, add save recycler list in file, add saveInsatance and delete item

--- app/src/main/java/com/example/lesson13/MainActivity.kt
+++ app/src/main/java/com/example/lesson13/MainActivity.kt
@@ -16,4 +16,109 @@ import java.io.ObjectOutputStream
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var toolbar: Toolbar
+    private lateinit var recyclerView: RecyclerView
+    private var arrayList = arrayListOf<Element>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initAll()
+    }
+
+    private fun initAll() {
+        toolbar = findViewById(R.id.main_toolbar)
+        recyclerView = findViewById(R.id.recycler)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setToolBar()
+        takeAllElementsFromFile()
+    }
+
+    private fun setToolBar() {
+        setSupportActionBar(toolbar)
+    }
+
+    private fun takeAllElementsFromFile(){
+        filesDir.listFiles().forEach { file ->
+            if (file.name == "data"){
+                val inputStream = openFileInput(file.name)
+                val objInputStream = ObjectInputStream(inputStream)
+                while (objInputStream.available()>0) {
+                    val title = objInputStream.readUTF()
+                    val count = objInputStream.readInt()
+                    arrayList.add(Element(title, count))
+                }
+                setRecycler()
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_screen_toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.save_btn -> saveInFile()
+            R.id.add_btn -> createItemOfRecycle()
+        }
+        return true
+    }
+
+    private fun setRecycler() {
+        with(recyclerView) {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = MainAdapter(arrayList,
+                onClick = {showAlert(it)})
+        }
+    }
+
+    private fun createItemOfRecycle() {
+        arrayList.add(Element("", 0))
+        setRecycler()
+    }
+
+    private fun saveInFile(){
+        if (arrayList.isNotEmpty()) {
+            try {
+                val fileOutput = openFileOutput("data", MODE_PRIVATE)
+                val objOutput = ObjectOutputStream(fileOutput)
+                arrayList.forEach {
+                    objOutput.writeUTF(it.title)
+                    objOutput.writeInt(it.currentCount)
+                }
+                objOutput.close()
+            } catch (e: IOException) {
+                Toast.makeText(this, "HERNYA", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun showAlert(element: Element){
+        val builder = AlertDialog.Builder(this@MainActivity)
+        builder.setPositiveButton("Yes") { _, _ ->
+            arrayList.remove(element)
+        }
+        builder.setNegativeButton("No") { _, _ -> }
+        builder.setTitle("Delete everything?")
+        builder.setMessage("Are you sure you want to delete everything?")
+        builder.show()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelableArrayList("THIS_LIST_OF_ITEMS", arrayList)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        arrayList = savedInstanceState
+            .getParcelableArrayList<Element>("THIS_LIST_OF_ITEMS") as ArrayList<Element>
+        setRecycler()
+    }
 }
--- app/src/main/java/com/example/lesson13/MainAdapter.kt
+++ app/src/main/java/com/example/lesson13/MainAdapter.kt
@@ -16,5 +16,55 @@ class MainAdapter(
     private val onClick:(Element) -> Unit
 ) :
     RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+    class ViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val title = item.findViewById<EditText>(R.id.title)!!
+        val deleteBtn = item.findViewById<ImageButton>(R.id.delete_btn)!!
+        private val plusBtn = item.findViewById<ImageButton>(R.id.plus_btn)!!
+        private val currentNumber = item.findViewById<TextView>(R.id.current_number)!!
+        private val minusBtn = item.findViewById<ImageButton>(R.id.minus_btn)!!
 
+        fun bind(element: Element , onClick: (Element) -> Unit) {
+            title.setText(element.title)
+            currentNumber.text = element.currentCount.toString()
+            plusBtn.setOnClickListener {
+                element.currentCount++
+                currentNumber.text = element.currentCount.toString()
+            }
+            minusBtn.setOnClickListener {
+                element.currentCount--
+                currentNumber.text = element.currentCount.toString()
+            }
+            title.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    element.title = title.text.toString()
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                }
+            })
+            deleteBtn.setOnClickListener {
+                onClick(element)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(arrayList[position], onClick)
+    }
+
+    override fun getItemCount(): Int = arrayList.size
 }
